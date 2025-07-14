da113e0e330167002e40fc972d211b9a6e981b08
Marek Kowolowski <mara2007cz@gmail.com>
2025-07-14 19:29:49 +0200

    Scikit learn, standardscaler

--- my_portfolio/road_map/day_10/scikit_learn.py
+++ my_portfolio/road_map/day_10/scikit_learn.py
@@ -6,14 +6,57 @@
 ####################################
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv("../data/train.csv")
+btc = yf.download("BTC-USD", start="2022-01-01", end="2025-01-01")
 
-features = ["Pclass", "Sex", "Age", "Fare"]
-target = "Survived"
+print("-"*120)
+print(btc.head())
+print("-"*120)
 
-df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
+btc["Close"].plot(figsize=(14, 5), title="Bitcoin zavírací cena")
+plt.xlabel("Datum")
+plt.ylabel("Cena v USD")
+plt.grid(True)
+plt.show()
 
-df = df["Age"].dropna()
+# Split dat na trénovací a testovací sadu
+btc = btc[["Close"]].dropna() # Odstraní řádky s chybějícími hodnotami
+
+btc["Tomorrow"] = btc["Close"].shift(-1)
+btc = btc.dropna()
+
+X = btc[["Close"]]
+y = btc["Tomorrow"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+
+# Normalizace dat (škálování)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Vytvoření modelu a natrénování
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test_scaled)
+
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+plt.figure(figsize=(14, 5))
+plt.plot(y_test.values, label="Sktuečné ceny")
+plt.plot(y_pred, label="Predikované ceny")
+plt.title("Predikce ceny Bitcoinu")
+plt.xlabel("Dny")
+plt.ylabel("Cena v USD")
+plt.legend()
+plt.grid(True)
+plt.show()
+
